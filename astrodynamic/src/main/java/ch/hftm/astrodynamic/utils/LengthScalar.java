a4e2f2b6a6d12ad50a60cd900a16b266d6e7deb5
Rafael Stauffer <rafael.staffer@hotmail.de>
2023-01-12 00:17:39 +0100

    multiplication and division with other dimensions

--- astrodynamic/src/main/java/ch/hftm/astrodynamic/utils/LengthScalar.java
+++ astrodynamic/src/main/java/ch/hftm/astrodynamic/utils/LengthScalar.java
@@ -24,4 +24,29 @@ public class LengthScalar extends BaseScalar {
         super(value, Unit.LENGTH);
     }
     
+    @Override
+    public Scalar multiply(Scalar scalar) throws UnitConversionError  {
+        switch (scalar.getUnit()) {
+            case LENGTH:
+                return new AreaScalar(getValue().multiply(scalar.getValue()));
+            case AREA:
+                return new VolumeScalar(getValue().multiply(scalar.getValue()));
+            case UNITLESS:
+                return new LengthScalar(getValue().multiply(scalar.getValue()));
+            default:
+                throw new UnitConversionError(String.format("Multiplication between %s and %s not possible in length scalar", getUnit().toString(), scalar.getUnit().toString()));
+        }
+    }
+
+    @Override
+    public Scalar divide(Scalar scalar) throws UnitConversionError  {
+        switch (scalar.getUnit()) {
+            case UNITLESS:
+                return new LengthScalar(getValue().divide(scalar.getValue()));
+            case LENGTH:
+                return new BaseScalar(getValue().divide(scalar.getValue()));
+            default:
+                throw new UnitConversionError(String.format("Division between %s and %s not possible in length scalar", getUnit().toString(), scalar.getUnit().toString()));
+        }
+    }
 }
