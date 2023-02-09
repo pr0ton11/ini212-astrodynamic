e294205827f78cffa3a2771af795644fec730adb
Marc Singer <ms@pr0.tech>
2023-02-09 21:31:26 +0100

    Created config repo

--- astrodynamic/src/main/java/ch/hftm/astrodynamic/MainApp.java
+++ astrodynamic/src/main/java/ch/hftm/astrodynamic/MainApp.java
@@ -7,16 +7,22 @@ package ch.hftm.astrodynamic;
  */
 
 import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import ch.hftm.astrodynamic.utils.ConfigRepository;
 
 // Taken from OOP1, Creates a stage for the first window, after that the controllers are self-governing
 public class MainApp extends Application {
     private Stage primaryStage;
 
     public static void main(String[] args) {
+        // Register args in Configuration repository
+        ConfigRepository.registerArgs(args);
+        // Launch application with args
         launch(args);
     }
 
