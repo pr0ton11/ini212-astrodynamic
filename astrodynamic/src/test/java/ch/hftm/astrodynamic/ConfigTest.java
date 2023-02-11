b6d604d28f52cd9da99dde781bef9b598ffb327e
Marc Singer <ms@pr0.tech>
2023-02-11 21:37:11 +0100

    Add configs

--- astrodynamic/src/test/java/ch/hftm/astrodynamic/ConfigTest.java
+++ astrodynamic/src/test/java/ch/hftm/astrodynamic/ConfigTest.java
@@ -4,8 +4,15 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+/*
+ *  Project Astrodynamic
+ *  HFTM BBIN21.2a
+ *  Rafael Stauffer, Marc Singer
+ */
+
+// Tests the ConfigurationRegistry
 public class ConfigTest {
 
     
-    
+
 }
