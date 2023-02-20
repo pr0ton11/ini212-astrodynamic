6f80463e25631d6637ca5c572a55b574409cf7f5
Marc Singer <ms@pr0.tech>
2023-02-20 14:36:27 +0100

    Finishing the serializer class

--- astrodynamic/src/main/java/ch/hftm/astrodynamic/utils/Serializer.java
+++ astrodynamic/src/main/java/ch/hftm/astrodynamic/utils/Serializer.java
@@ -1,12 +1,12 @@
 package ch.hftm.astrodynamic.utils;
 
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.Version;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -34,18 +34,33 @@ public class Serializer {
             // Enable detection of fields of any visibility, allows privte fields to be serialized
             om.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
         
+            // This should not be required for our use case
             // om.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
         }
     }
 
     // Loads the MissionRepository Object from a path
     public static void fromFile(String path) {
-
+        try {
+            // Read file with embedded file parser
+            MissionRepository mr = om.readValue(path, MissionRepository.class);
+            // Update current mission repository from path
+            MissionRepository.overrideInstance(mr);
+        } catch (JsonProcessingException ex) {
+            log.severe(String.format("Could not load MissionRepository from file %s:", path));
+            log.severe(ex.toString());
+        }
     }
 
     // Writes the MissionRepository Object to a path
     public static void toFile(String path) {
-
+        try {
+            // Write file with embedded file parser
+            om.writeValue(Paths.get(path).toFile(), MissionRepository.getInstance());
+        } catch (Exception ex) {
+            log.severe(String.format("Could not write MissionRepository to file %s:", path));
+            log.severe(ex.toString());
+        }
     }
 
     // Writes the MissionRepository Object to a string
@@ -55,6 +70,7 @@ public class Serializer {
         try {
             return om.writeValueAsString(MissionRepository.getInstance());
         } catch (JsonProcessingException ex) {
+            log.severe("Could not serialize MissionRepository to string");
             log.severe(ex.toString());
             return "";
         }
@@ -67,6 +83,7 @@ public class Serializer {
         try {
             return om.readValue(element, MissionRepository.class);
         } catch (JsonProcessingException ex) {
+            log.severe(String.format("Could not load MissionRepository from string %s:", element));
             log.severe(ex.toString());
             return null;
         }
@@ -79,6 +96,7 @@ public class Serializer {
         try {
             return om.readValue(element, Mission.class);
         } catch (JsonProcessingException ex) {
+            log.severe(String.format("Could not load Mission from string %s:", element));
             log.severe(ex.toString());
             return null;
         }
@@ -91,6 +109,7 @@ public class Serializer {
         try {
             return om.writeValueAsString(mission);
         } catch (JsonProcessingException ex) {
+            log.severe("Could not serialize Mission to string");
             log.severe(ex.toString());
             return "";
         }
